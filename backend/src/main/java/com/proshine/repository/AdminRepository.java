62d848f66efc4afd998630d8ca7c5bdf7289445c
jiang666 <934321800@qq.com>
2025-07-23 16:31:47 +0800

    123

--- backend/src/main/java/com/proshine/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proshine.repository;
-
-import com.proshine.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
