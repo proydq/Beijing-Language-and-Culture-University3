2d963d28f9687ade28be154810dc1323e4a32c83
jiang666 <934321800@qq.com>
2025-07-23 18:04:41 +0800

    123

--- backend/src/main/java/com/proshine/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proshine.repository;
-
-import com.proshine.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
